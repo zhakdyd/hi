84c503aab2c170526d22258dbc641ab3921f45d9
nakyong234 <nakyong234@naver.com>
2019-09-28 18:00:39 +0900

    dd

--- st05.JavaScript/js03.프로그래밍기초/js03-01.js
+++ st05.JavaScript/js03.프로그래밍기초/js03-01.js
@@ -1,6 +1,9 @@
 "use strict";
 
+//대소문자를 구분한다.
 var i = 10;
-var I = 10;
-
+var I = 10; //숫자 Number
+var I = "10" //문자열 String
+var I = true; //블린 (있다, 없다), Boolean
+//위에서부터 아래로 쭉 내려오면서, 마지막 것이 적용됨
 
